82cfa6ceb8ddccc206085c605197e64d7ace2837
Aye Chan Wai <ayechanwai.edu@gmail.com>
2024-06-11 09:35:16 +0630

    first

---
+++ bin/@1_dart_basic.dart
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+// void main(List<String> args) {
+//   for (var i = 0; i < 10; i++) {
+//     print("Test loop => $i");
+//   }
+// }
+
+
+// void main(){
+//   print('Hello world');
+// }
---
+++ bin/dart_basic.dart
@@ -0,0 +1,10 @@
+import 'package:dart_basic/dart_basic.dart' as dart_basic;
+
+// void main(){
+//   print('Hello world');
+// }
+
+// void main(List<String> arguments) {
+//   print('Hello world: ${dart_basic.calculate()}!');
+// }
+
